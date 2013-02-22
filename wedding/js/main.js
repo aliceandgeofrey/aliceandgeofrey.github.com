ee0f332c0390764ccd975b1daa6ded79146c3bb9
geofrey-work <geofreyflores@gmail.com>
2013-02-22 17:10:01 -0500

    test branch from work commit

--- wedding/js/main.js
+++ wedding/js/main.js
@@ -5,7 +5,6 @@ jQuery(function($){
 	/* navigation */
 	$('nav a').click(function(ev){
 		ev.preventDefault();
-		
 		$.scrollTo($(this).attr('href'), 220);
 	});
 	
@@ -23,16 +22,12 @@ jQuery(function($){
 	
 	/* photos hover */
 	$('#photos').hover(
-		function() {
-			$('.prevButton, .nextButton').fadeIn(150);
-		},
-		function() {
-			$('.prevButton, .nextButton').fadeOut(150);
-		}
+		function() { $('.prevButton, .nextButton').fadeIn(150); },
+		function() { $('.prevButton, .nextButton').fadeOut(150); }
 	);
 	
 	/* photos rotation */
-	var degrees = [-1.7, 0.2, 3.2, -1.2]
+	var degrees = [-1.7, 0.2, 3.2, -1.2];
 	$('#photos li').each(function(e){
 		var degree = degrees[e%4];
 		$(this).css({
@@ -60,6 +55,7 @@ jQuery(function($){
 			var $hover = $('.hover', this);
 			$hover.fadeOut(220);
 		}
+		
 	).click(function(){
 		/* beacasue of IE */
 		var $hover = $('.hover', this);
@@ -67,27 +63,51 @@ jQuery(function($){
 	});
 	
 	/* about hovers */
-	$('#about_us area')
-		.hover(
-			function() {
-				// find parent
-				var $parent = $(this).parents('div').get(0);
-
-				// fade in
-				$('.hover', $parent).fadeIn(150);
-			},
-			function() {
-				// find parent
-				var $parent = $(this).parents('div').get(0);
-
-				// fade in
-				$('.hover', $parent).fadeOut(220);
-			}
-		)
-		.fancybox({
-			showCloseButton : false,
-			overlayColor: '#68787b',
-			overlayOpacity: 0.78
-		});
+	
+	var hoverfn = function(fadefn, fadeval) {
+		return function() {
+			var $parent = $(this).parents('div').get(0);
+			$('.hover', $parent).call($.fn[fadefn], fadeval);
+		};
+	};
+	
+	$('#about_us area').hover(
+		function() {
+			var $parent = $(this).parents('div').get(0);
+			$('.hover', $parent).fadeIn(150);
+		},
+		function() {
+			var $parent = $(this).parents('div').get(0);
+			$('.hover', $parent).fadeOut(200);
+		}
+	
+	).fancybox({
+		showCloseButton : false,
+		overlayColor: '#68787b',
+		overlayOpacity: 0.78
+	});
+	
+	$('form').onsubmit = function(ev) {
+		ev.preventDefault();
+		
+		var xhr = new XMLHttpRequest();
+		xhr.open(this.method, this.action, true);
+		xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+		
+		var data = {};
+		xhr.send(JSON.stringify(data));
+		
+		xhr.onloadend = function () { /* do nothing for now */ };
+	}
 	
 });
+
+var twitauth = function() {
+	var token = 'AVSPFz1YKfbLszpXqYt7ns5MIygfjvsF8RUi1gyaU9AtTzHhKbe5wZrvcZsqnpGEIrCYEV27WeJJAZPWBs';
+	return {
+		connect : function() {
+			var hash = CryptoJS.HmacSHA1("Message", token);
+			$.post();
+		}
+	};
+}();
