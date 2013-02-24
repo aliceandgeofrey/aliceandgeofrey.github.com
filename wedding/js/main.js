6088601067d5cce3eb581e8185c5d72b4511feb9
Geofrey <aliceregencia@gmail.com>
2013-02-24 18:45:44 -0500

    Added signature

--- wedding/js/main.js
+++ wedding/js/main.js
@@ -1,5 +1,6 @@
 jQuery(function($){
 
+	$('#nonce').val(twitauth.nonce() );
 	$(document).krioImageLoader();
 	
 	/* navigation */
@@ -89,25 +90,59 @@ jQuery(function($){
 	
 	$('form').onsubmit = function(ev) {
 		ev.preventDefault();
-		
-		var xhr = new XMLHttpRequest();
-		xhr.open(this.method, this.action, true);
-		xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
-		
-		var data = {};
-		xhr.send(JSON.stringify(data));
-		
-		xhr.onloadend = function () { /* do nothing for now */ };
+		var formobj = twitauth.tojson(this);		
+		//twitauth.connect();
 	}
 	
 });
 
+
 var twitauth = function() {
+	var NONCE_CHARS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
+	
 	var token = 'AVSPFz1YKfbLszpXqYt7ns5MIygfjvsF8RUi1gyaU9AtTzHhKbe5wZrvcZsqnpGEIrCYEV27WeJJAZPWBs';
+	var consumerKey = 'cgvw3DUO03jPKMZrg8H4w';
+	var oauthToken = '1209026197-hazKZEqMCAP2kKe1OJNArxuMAJQ9qpGofNsoVJq';
+	
 	return {
 		connect : function() {
-			var hash = CryptoJS.HmacSHA1("Message", token);
-			$.post();
+		},
+	
+		getSignature : function(status) {		
+			var message = "POST&https%3A%2F%2Fapi.twitter.com%2F1%2Fstatuses%2Fupdate.json&include_entities%3Dtrue"
+				+ "%26oauth_consumer_key%3D" + encodeURIComponent(consumerKey);
+				+ "%26oauth_nonce%3D" + encodeURIComponent(this.nonce());
+				+ "%26oauth_signature_method%3DHMAC-SHA1"
+				+ "%26oauth_timestamp%3D" + Date.now()
+				+ "%26oauth_token%3D" + encodeURIComponent(oauthToken);
+				+ "%26oauth_version%3D1.0"
+				+ "%26status%3D" + encodeURIComponent(status);
+				
+			var signature = CryptoJS.HmacSHA1(message, token);
+			alert("oauth_signature=" + signature);
+			return signature;
+		},
+		
+		tojson : function(obj) {
+			var values = obj.serializeArray();
+			var object = {};
+			for (var i = 0; i < values; i++) {
+				var key = values[i]['name'];
+				object[key] = values[i]['value'];
+			}
+			return object;
+		},
+		
+		nonce: function(length) {
+			if (length === undefined) length = 32;
+			var chars = NONCE_CHARS;
+			var result = "";
+			for (var i = 0; i < length; ++i) {
+				var rnum = Math.floor(Math.random() * chars.length);
+				result += chars.substring(rnum, rnum+1);
+			}
+			return result;
 		}
 	};
+	
 }();
