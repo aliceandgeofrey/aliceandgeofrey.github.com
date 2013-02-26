c0cc5cdffca949df724066fef0c0dbc102094a67
Geofrey <aliceregencia@gmail.com>
2013-02-25 23:00:53 -0500

    Fixed  google form

--- wedding/js/main.js
+++ wedding/js/main.js
@@ -1,6 +1,5 @@
 jQuery(function($){
 
-	$('#nonce').val(twitauth.nonce() ); // generate nonce value per load
 	$(document).krioImageLoader();
 	
 	/* navigation */
@@ -65,13 +64,6 @@ jQuery(function($){
 	
 	/* about hovers */
 	
-	var hoverfn = function(fadefn, fadeval) {
-		return function() {
-			var $parent = $(this).parents('div').get(0);
-			$('.hover', $parent).call($.fn[fadefn], fadeval);
-		};
-	};
-	
 	$('#about_us area').hover(
 		function() {
 			var $parent = $(this).parents('div').get(0);
@@ -90,113 +82,48 @@ jQuery(function($){
 	
 	$(":submit").click(function() {
 		$('input#response').val($(this).val() );
+		formutil.validateForm();
 	});
 	
-	$('form').submit(function(ev) {
-		return true;
-	});
+	$('form').submit(function() { return true; });
 	
 });
 
 
-var sendform = function() {
-
-	return {
-		tojson : function(obj) {
-			var values = obj.serializeArray();
-			var object = {};
-			for (var i = 0; i < values.length; i++) {
-				var key = values[i]['name'];
-				object[key] = values[i]['value'];
-			}
-			return object;
-		},
-	};
-}();
-
-
-
-
-
-
-
-
-var twitauth = function() {
-	var NONCE_CHARS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
-	
-	var token = 'AVSPFz1YKfbLszpXqYt7ns5MIygfjvsF8RUi1gyaU9AtTzHhKbe5wZrvcZsqnpGEIrCYEV27WeJJAZPWBs';
-	var consumerKey = 'cgvw3DUO03jPKMZrg8H4w';
-	var oauthToken = '1209026197-hazKZEqMCAP2kKe1OJNArxuMAJQ9qpGofNsoVJq';
-	
-	return {
-		connect : function(form, statusfn) {
-			if (!(form instanceof $)) form = $(form);
-			
-			var formobj = twitauth.tojson(form);
-			formobj.status = statusfn(formobj);
-			
-			twitauth.setSignature(formobj);
-			var authHeader = twitauth.getAuthHeader(formobj);
-			console.log(authHeader);
-			
-			$.ajax({
-				url: 'https://api.twitter.com' + form.attr('action'),
-				type: 'POST',
-				data: "status=" + encodeURIComponent(formobj.status),
-				sucess: function(resp) { alert(JSON.stringify(resp)); },
-				error: function(resp) { alert(JSON.stringify(resp)); },
-				headers : { 'Access-Control-Allow-Origin' : '*' },
-				beforeSend: function(xhr) {  
-					xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
-					xhr.setRequestHeader('Authorization', authHeader);
-				}
-			});
-		},
-		
-		getAuthHeader : function(formObj) {
-			return 'OAuth oauth_consumer_key="' + encodeURIComponent(consumerKey)
-				+ '", oauth_nonce="' + encodeURIComponent(formObj.nonce)
-				+ '", oauth_signature="' + encodeURIComponent(formObj.signature)
-				+ '", oauth_signature_method="HMAC-SHA1'
-				+ '", oauth_timestamp="' + encodeURIComponent(formObj.timestamp)
-				+ '", oauth_token="' + encodeURIComponent(oauthToken)
-				+ '", oauth_version="1.0"';
-		},
-	
-		setSignature : function(formObj) {
-			formObj.timestamp = Date.now();
-			var message = "POST&https%3A%2F%2Fapi.twitter.com%2F1%2Fstatuses%2Fupdate.json&include_entities%3Dtrue"
-				+ "%26oauth_consumer_key%3D" + encodeURIComponent(consumerKey)
-				+ "%26oauth_nonce%3D" + encodeURIComponent(formObj.nonce)
-				+ "%26oauth_signature_method%3DHMAC-SHA1"
-				+ "%26oauth_timestamp%3D" + formObj.timestamp
-				+ "%26oauth_token%3D" + encodeURIComponent(oauthToken)
-				+ "%26oauth_version%3D1.0"
-				+ "%26status%3D" + encodeURIComponent(formObj.status);
-			formObj.signature = CryptoJS.HmacSHA1(message, token).toString(CryptoJS.enc.Base64);
-			console.log(formObj.signature.toString());
-		},
-		
-		tojson : function(obj) {
-			var values = obj.serializeArray();
-			var object = {};
-			for (var i = 0; i < values.length; i++) {
-				var key = values[i]['name'];
-				object[key] = values[i]['value'];
-			}
-			return object;
-		},
-		
-		nonce: function(length) {
-			if (length === undefined) length = 32;
-			var chars = NONCE_CHARS;
-			var result = "";
-			for (var i = 0; i < length; ++i) {
-				var rnum = Math.floor(Math.random() * chars.length);
-				result += chars.substring(rnum, rnum+1);
-			}
-			return result;
+var formutil = function() {
+	var show = {
+		t : function(aClass, rClass) {
+			return function(newmsg) {
+			var msg = $('span#message');
+			msg.removeClass(rClass).addClass(aClass);
+			msg.text(newmsg); }
 		}
 	};
+	show.error = show.t('error', 'success');
+	show.success = show.t('success', 'error');
 	
+	return {
+		validateForm : function() {
+			var obj = formutil.tojson($('form'));
+			var v = obj['entry.1291690228']; // name field
+			if (v === undefined || v.trim() === "") {
+				show.error('please fill in your name');
+			} else {
+				show.success('thank you for responding!');
+			}
+		},
+		
+		tojson : function(obj) {
+			var j = {};
+			
+			var fields = obj.serializeArray();
+			for (var i = 0; i < fields.length; i++) {
+				var v = fields[i].name;
+				j[v] = fields[i].value;
+			}
+			return j;
+		}
+	}
 }();
+
+
